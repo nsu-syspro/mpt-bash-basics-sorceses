53de336742a4fdbc3a82a4a0eb27ef407303d5b8
Sorce <sorce6285@gmail.com>
2025-09-30 20:50:40 +0700

    untar.sh

--- solution/untar.sh
+++ solution/untar.sh
@@ -1,13 +1,4 @@
-#!/bin/bash
-
-set -e
-if [[ "$#" -ne 1 ]]; then
-    echo "usage: $0 <archive>"
-    exit 1
-fi
-
-archive="$1"
-dir="$archive.unpacked"
-
+#1
+dir="${1}.unpacked"
 mkdir -p "$dir"
-tar -xf "$archive" -C "$dir"
+tar -xf "$1" -C "$dir"
